40329532659caa06e0a4ed5ad78f7cf606d5df35
unknown <C:\Users\ i\AppData\Roaming\The Bat!\Mail>
2022-01-15 14:59:14 +0200

    init

---
+++ HW_19/HW_19/UnitTest1.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+using OpenQA.Selenium;
+
+
+namespace HW_19
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
